6c9043a513b61bf5edb7d9e5278862b3fbfab116
Viktor <viktor@maigaard.io>
2019-04-06 14:56:27 +0200

    Try this Elias

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
 const five = require("johnny-five")
 const board = new five.Board();
 
-board.on("ready", () => {
+board.on("ready", function() {
 
+    const temperature = new five.Thermometer({
+        controller: "TMP102"
+    });
+
+    temperature.on("data", () => {
+        console.log("celsius: %d", this.C);
+        console.log("fahrenheit: %d", this.F);
+        console.log("kelvin: %d", this.K);
+    });
 });
