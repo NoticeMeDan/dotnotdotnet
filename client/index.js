d0046fcd1ebdeb53ed045d4ee0f166853010a6f2
Viktor <viktor@maigaard.io>
2019-04-06 15:14:54 +0200

    moist me up baby

--- client/index.js
+++ client/index.js
@@ -12,4 +12,11 @@ board.on("ready", function() {
         console.log("fahrenheit: %d", data.F);
         console.log("kelvin: %d", data.K);
     });
+
+    const sensor = new five.Sensor("A0");
+
+    // Scale the sensor's data from 0-1023 to 0-10 and log changes
+    sensor.on("change", scaleTo => {
+        console.log(scaleTo(0, 10));
+    });
 });
